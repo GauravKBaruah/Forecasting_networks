6e4a45d3d4c6ef26ee6913eda25f45d60ccd3af5
Panthera-lupus <87260688+Panthera-lupus@users.noreply.github.com>
2023-12-18 10:50:25 +0100

    Add files via upload

--- run_sim_increasing_temp_rate_env_var.R
+++ run_sim_increasing_temp_rate_env_var.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(readr)
 library(beepr)
 library(viridis)
+library(wesanderson)
 
 # change if necessary
 network_dir = '../networks_sizes_8_34'
@@ -21,9 +22,9 @@ fact <- expand.grid(Temperature=20,
                     connectance = 0,
                     nestedness = 0,
                     h2 = c("none", "rand"),
-                    rate = seq(0.05, 0.2, 0.025),
-                    env_var = c(0.25, 2.5, 5),                                 
-                    `replicates`=1+(1:3)*100
+                    rate = seq(0.075, 0.2, 0.025),
+                    env_var = seq(0, 7.5, 1.5),                                 
+                    `replicates`=1+(1:10)*100
                     ) %>% arrange_all()
 
 for (r in 1:nrow(fact)){
@@ -40,9 +41,9 @@ for (r in 1:nrow(fact)){
 extinction_tp_list <- list(rep(0, (fact$network_size)[1]))
 
 for (i in 1:10) {
-  for(j in 1:126) {
-    network_size <- (fact$network_size)[i*126]
-    extinction_tp_list[[(i-1)*126 + j]] <- rep(0, network_size)
+  for(j in 1:720) {
+    network_size <- (fact$network_size)[i*720]
+    extinction_tp_list[[(i-1)*720 + j]] <- rep(0, network_size)
   }
 }
 
@@ -50,7 +51,7 @@ fact <- fact %>% mutate(extinction_tps = extinction_tp_list)
 
 # simulation of time series
 system.time(for(r in 1:nrow(fact)){
-  
+  print(r)
   g<-adj.mat(webfiles[which(webfiles == fact$web[r])])  
   
   Aspecies <- dim(g)[2]        
@@ -89,7 +90,7 @@ system.time(for(r in 1:nrow(fact)){
   if (fact$h2[r] == "none") {
     h2 <- rep(0, Aspecies + Pspecies)
   } else {
-      h2 <- runif(Aspecies + Pspecies, 0.25, 0.4)
+      h2 <- runif(Aspecies + Pspecies, 0.2, 0.4)
   }
   
   #parameters below are taken from another paper Akesson et al 2021 Nat Comms.
@@ -104,7 +105,7 @@ system.time(for(r in 1:nrow(fact)){
   Pmatrix <- mat.comp(g)$Pmatrix                                                # competition matrix , aij, for plants
   mut.strength = 1.5                                                            # average mutualistic strength
                                                 
-  tmax <- 5e3                                                                    # time to integrate equations for
+  tmax <- 5e3                                                                   # time to integrate equations for
   dt <- 0.1
   initial_temperature <- fact$Temperature[1]
   
@@ -118,7 +119,7 @@ system.time(for(r in 1:nrow(fact)){
   
   ic <- c(na,np,muA,muP)                                                        # initial conditions coerced into a vector
   
-  out <- eqs_euler_2(time = tmax, y = ic, pars = params)                          # generation of time series
+  out <- eqs_euler(time = tmax, y = ic, pars = params)                        # generation of time series
   fact <- calculate_extinction_tps(out, network_size, fact, r)                  
   
   #------------     Quick testing    -------------------------------------------
@@ -131,88 +132,36 @@ system.time(for(r in 1:nrow(fact)){
   
 })
 
-saveRDS(fact, "increasing_fluctuating_231128.rds")
+# adjust date 
+saveRDS(fact, "increasing_fluctuating_231204.rds")
 
 #-------------------------------------------------------------------------------
-
-#     plotting
-
+#
+#                  plotting time series
+#
 #-------------------------------------------------------------------------------
 
-myColors = c("#FF0000", "#00FF00", "#0000FF",
-             "#FFFF00", "#FF00FF", "#00FFFF",
-             "#333333", "#777777", "#BFFFBB",
-             "#FFCCCC", "#CCFFCC", "#CCCCFF",
-             "#FFFFCC", "#FFCCFF", "#CCFFFF",
-             "plum", "orange", "wheat4", "brown") 
+out <- readRDS(file = "M_PL_061_33_rand_0.075_1.5_101_time_series_231205.rds" ) # time series to plot 
 
+#------------------------------------------------------------
+#------------------------------------------------------------
+#                  color palettes                                               
+#------------------------------------------------------------
+#------------------------------------------------------------
 
-ts.plot(out$Na, gpars = list(xlab = paste("increasing", networkString, "high", fact$h2[r],  "rate: ", rate, "env_var: ", env_var, sep = ", "), ylab = "abundance, animals", col = myColors)) 
-ts.plot(out$Np, gpars = list(xlab = paste("increasing", networkString, "high", fact$h2[r],  "rate: ", rate, "env_var: ", env_var, sep = ", "), ylab = "abundance, plants", col = myColors))
-ts.plot(out$Temp, gpars = list(xlab = paste("increasing", networkString, "high", fact$h2[r],  "rate: ", rate, "env_var: ", env_var, sep = ", "), ylab = "abundance, animals", col = myColors)) 
+diverseColors = c("#FF0000", "#00FF00", "#0000FF",
+                  "#FFFF00", "#FF00FF", "#00FFFF",
+                  "#333333", "#777777", "#BFFFBB",
+                  "#FFCCCC", "#CCFFCC", "#CCCCFF",
+                  "#FFFFCC", "#FFCCFF", "#CCFFFF",
+                  "plum", "orange", "wheat4", "brown")
 
+animalColors = c("#FF0000", "#FF8B00", "#BA5430", "#FFD100", "#FF5D00",
+                 "#FF7400", "#AF1400", "#FFA200", "#FFB900", "#FFFF80",
+                 "#FFE800", "#FFFF00", "#FFFF2A", "#FA4680", "#FFFFD5")
 
-#------------------------------------------------------------------------------
-#
-#         miscellaneous
-#
-#------------------------------------------------------------------------------
+plantColors = c("#00A600", "#2DB600", "#63C600", "#A0D600", "#E6E600",
+                "#E8C32E", "#EBB25E", "#EDB48E", "#F0C9C0", "#52F252")
 
-# system.time()
-
-# ggplot(data = as_tibble(out$Na)) 
-#   geom_line(mapping = aes(x = out$Temp, y = , color = factor(h2)))
-# 
-# ggplot(data = dat_01) +
-#   geom_point(mapping = aes(x = temperature, y = plant_richness, color = factor(h2)))
-# 
-# ggplot(data = dat_01) +
-#   geom_point(mapping = aes(x = temperature, y = animal_richness, color = factor(h2)))
-
-# plot.ts(out$Na, ylab = "abundance, animals")
-# plot.ts(out$Np, ylab = "abundance, plants")
-# plot.ts(out$muA, ylab = "trait, animals")
-# plot.ts(out$muP, ylab = "trait, plants")
-# plot.ts(out$Temp, ylab = "Temperature[°C]")
-
-#------------------------------------------------------------------------
-
-# ggplot(networkData$Na, aes(x = Time, y = )) + 
-#   geom_line(aes(color = variable), size = 1) +
-#   scale_color_manual(values = myColors) +
-#   theme_minimal()
-
-#save(out, file ="../output/warming_increasing_temp.RData")
-
-# https://www.climate.gov/news-features/understanding-climate/climate-change-global-temperature
-#--> 5/9 * 6 projected increase in temperature between 2021 and 101 
-
-#------------------------------------------------------------------------
-
-# fact_2<- expand.grid(`web` =webfiles[1:153]) %>%
-#   as_tibble %>%
-#   mutate(`Nestedness`= 0,
-#          `Connectance` = 0,
-#          `network_size`=0)
-# # a loop to go over the rows of the dataframe to calculate nestedness and network size for each network
-# for (r in 1:nrow(fact_2)){
-#   g<-adj.mat(myfiles[which(myfiles == fact_2$web[r])]) #network web names
-#   Aspecies<- dim(g)[2] # no of animal species
-#   Plantspecies<- dim(g)[1]
-#   
-#   fact_2$network_size[r]<-Aspecies+Plantspecies
-#   fact_2$Nestedness[r]<-  round(nestedness_NODF(g),2)
-#   fact_2$Connectance[r] <- Connectance(g)
-# }- 
-
-# a loop to go over the rows of the dataframe to calculate nestedness and network size for each network
-# fact_2 <- expand.grid(`web` = webfiles) %>% #153]) %>% rate, variance
-#   as_tibble %>%
-#   mutate(`Nestedness`= 0,
-#          `Connectance` = 0,
-#          `network_size`= 0)
-
-# ts.plot(networkData$muA, gpars = list(xlab = paste("increasing", network, var_string, h2_string, sep = ", "), ylab = "trait, animals", col = myColors))
-# ts.plot(networkData$muP, gpars = list(xlab = paste("increasing", network, var_string, h2_string, sep = ", "), ylab = "trait, plants", col = myColors))
-# ts.plot(networkData$Temp, gpars = list(xlab = paste("increasing", network, var_string, h2_string, sep = ", "), ylab = "temperature", col = myColors))
+ts.plot(cbind(out$Na[0:900,], out$Np[0:900, ]), gpars = list(xlab = "time", ylab = "abundance", col = c(animalColors[1:6], plantColors[1:2])), lwd = 2)
 
